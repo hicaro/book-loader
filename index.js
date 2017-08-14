1df451dbea631088abcb4f9b428f4accd84f34f0
hicaro <hicaro.adriano@gmail.com>
2017-08-14 15:29:33 -0400

    added fast-csv parser

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var readline  = require('readline');
+var csv       = require('fast-csv');
 var fs        = require('fs');
 var db        = require('./db');
 var Books     = require('./books');
@@ -7,39 +7,37 @@ var Books     = require('./books');
 db.connect();
 
 var load = function () {
-  var reader = readline.createInterface({
-    input: fs.createReadStream('books.csv')
-  });
+  var stream = fs.createReadStream('books.csv');
 
   var lineCouter = 0;
   var header = [];
 
-  reader.on('line', function(line) {
-    var items = line.split('|');
-
-    if (lineCouter == 0) {
-      header = items;
-      lineCouter++;
-    } else {
-      var book = {};
-
-      if (header.length === items.length) {
-        header.forEach(function (key, i) {
-          book[key] = items[i];
-        });
-
-        Books.insert(book);
+  var csvStream = csv
+    .parse({delimiter: '|'})
+    .on("data", function(items){
+      if (lineCouter == 0) {
+        header = items;
         lineCouter++;
+      } else {
+        var book = {};
+
+        if (header.length === items.length) {
+          header.forEach(function (key, i) {
+            book[key] = items[i];
+          });
+
+          Books.insert(book);
+          lineCouter++;
+        }
       }
-    }
+    })
+    .on("end", function(){
+      console.log((lineCouter - 1) + ' books were imported.');
+      // close database connection
+      db.disconnect();
+    });
 
-  });
-
-  reader.on('close', function(){
-    console.log((lineCouter - 1) + ' books were imported.');
-    // close database connection
-    db.disconnect();
-  });
+  stream.pipe(csvStream);
 };
 
 // remove old values and insert new ones
