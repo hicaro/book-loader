6d297efc78bfbee038025dccc8755d64db83fc82
hicaro <hicaro.adriano@gmail.com>
2017-08-14 11:57:59 -0400

    added flux control

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-var readline = require('readline');
-var fs = require('fs');
+var readline  = require('readline');
+var fs        = require('fs');
+var db        = require('./db');
+var Books     = require('./books');
 
-var reader = readline.createInterface({
-  input: fs.createReadStream('books.txt')
-});
+// connect to database
+db.connect();
 
-reader.on('line', function(line) {
-  var items = line.split('|');
-  console.log(items);
-});
+var load = function () {
+  var reader = readline.createInterface({
+    input: fs.createReadStream('books.csv')
+  });
+
+  var lineCouter = 0;
+  var header = [];
+
+  reader.on('line', function(line) {
+    var items = line.split('|');
+
+    if (lineCouter == 0) {
+      header = items;
+    } else {
+      var book = {};
+
+      header.forEach(function (key, i) {
+        book[key] = items[i];
+      });
+
+      Books.insert(book);
+    }
+
+    lineCouter++;
+  });
+
+  reader.on('close', function(){
+    console.log((lineCouter - 1) + ' books were imported.');
+    // close database connection
+    db.disconnect();
+  });
+};
+
+// remove old values and insert new ones
+Books.cleanImport(load);
