c9ec2b08be9b472a3a4bf95b76664e258c67e79f
hicaro <hicaro.adriano@gmail.com>
2017-08-15 13:09:46 -0400

    added replacement for smart quotes

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ var load = function () {
 
         if (header.length === items.length) {
           header.forEach(function (key, i) {
-            book[key] = items[i];
+            book[key] = items[i]
+              .replace(/[\u2018\u2019]/g, "'")
+              .replace(/[\u201C\u201D]/g, '"')
+              .replace(/[\u2013\u2014]/g, '-')
+              .replace(/[\u2026]/g, '...');
           });
 
           Books.insert(book);
