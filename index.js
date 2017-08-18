30f77eca2e86b538ec1d4ab6890fbe0bf43502aa
hicaro <hicaro.adriano@gmail.com>
2017-08-17 20:02:36 -0400

    trim value

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ var load = function () {
               .replace(/[\u2018\u2019]/g, "'")
               .replace(/[\u201C\u201D]/g, '"')
               .replace(/[\u2013\u2014]/g, '-')
-              .replace(/[\u2026]/g, '...');
+              .replace(/[\u2026]/g, '...')
+              .trim();
           });
 
           Books.insert(book);
